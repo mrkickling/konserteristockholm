3294bd9a5c6305a5fc02dab84edb151805c71796
Joakim Loxdal <joakim.loxdal@orangecyberdefense.com>
2023-12-29 17:44:17 +0100

    Add billetto

---
+++ concert_scraper/scraper/modules/billetto.py
@@ -0,0 +1,61 @@
+"""Fetch data from billetto.se"""
+
+from bs4 import BeautifulSoup
+from common import Concert
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+import time
+
+BASE_URL = "https://billetto.se"
+By.CSS_SELECTOR
+
+
+def get_concerts(venue, browser):
+    browser.get(venue.url)
+    time.sleep(5)
+    concerts = set()
+
+    try:
+        cookie_button = browser.find_element(By.ID, "onetrust-accept-btn-handler")
+        cookie_button.click()
+    except NoSuchElementException:
+        print("No cookie banner")
+
+    while True:
+        # Go through all pages
+        html = browser.page_source
+
+        def clean_date(date_str):
+            dates = date_str.split("-")
+            start_date = dates[0].strip()
+            return start_date
+
+        soup = BeautifulSoup(html, features="html.parser")
+        classes = "bg-white dark:bg-gray-800 flex flex-col justify-between relative border-gray-200 dark:border-gray-700 sm:rounded-lg divide-y divide-gray-200 dark:divide-gray-700 border shadow overflow-hidden"
+        cards = soup.find_all(name="div", attrs={'class': classes})
+        for card in cards:
+            title_attrs = {'class': 'block text-white hover:text-brand-500 font-medium text-base truncate'}
+            concert_title = card.find('a', attrs=title_attrs)
+            if not concert_title or not len(concert_title.getText()):
+                continue
+            concert_title = concert_title.getText()
+
+            date_attrs = {'class': 'border border-gray-200 p-1 text-xs text-brand-500 bg-white rounded-md'}
+            concert_dates = clean_date(card.find(attrs=date_attrs).get_text())
+
+            concert_url = card.find('a').get('href') if card.find('a') else venue.url
+            concerts.add(
+                Concert(concert_title, concert_dates, venue.name, concert_url)
+            )
+
+        # Next page
+        try:
+            button = browser.find_element(By.CSS_SELECTOR, "[aria-label=Nästa]")
+            if button.is_enabled():
+                button.click()
+            else:
+                break
+        except NoSuchElementException:
+            break
+
+    return concerts
