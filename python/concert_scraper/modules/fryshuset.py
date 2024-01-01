66467ee11b67edc667c9bfddfe449879443ecfb7
mrkickling <loxdalen@gmail.com>
2024-01-01 19:56:23 +0100

    feat: add fryshuset scraper module

---
+++ python/concert_scraper/modules/fryshuset.py
@@ -0,0 +1,60 @@
+# For https://fryshuset.se/konserter
+
+from bs4 import BeautifulSoup
+from selenium.webdriver.common.by import By
+import time
+from datetime import datetime
+
+from concert_scraper.common import Concert
+from concert_scraper.logger import get_logger
+
+logger = get_logger(__name__)
+
+BASE_URL = "https://fryshuset.se/konserter/"
+
+def parse_date(date_string):
+    # '28 feb 2024'
+    months_se = [
+        "jan",
+        "feb",
+        "mar",
+        "apr",
+        "maj",
+        "jun",
+        "jul",
+        "aug",
+        "sep",
+        "okt",
+        "nov",
+        "dec"
+    ]
+    date, month, year = date_string.split()
+    date_int = int(date)
+    month_int = months_se.index(month.lower()) + 1
+    year_int = int(year)
+    return datetime(year_int, month_int, date_int).strftime("%Y-%m-%d")
+
+def get_concerts(venue, browser):
+    logger.info(f"Getting concerts for venue {venue.name}")
+
+    browser.get(venue.url)
+    time.sleep(1) # Wait for cookie dialog
+    browser.find_element(By.ID, 'CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll').click()
+    browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(1) # Wait for more to load
+    html = browser.page_source
+
+    soup = BeautifulSoup(html, features="html.parser")
+    events = soup.find_all("div", attrs={'class': 'event-description'})
+
+    concerts = []
+    for concert in events:
+        concert_title = concert.find('span', attrs={'class': 'event-title'}).getText().strip()
+        concert_date = parse_date(concert.find('span', attrs={'class': 'event-date'}).getText().strip())
+        concert_url = BASE_URL
+        concerts.append(
+            Concert(concert_title, concert_date, venue.name, concert_url)
+        )
+
+    logger.info(f"Found {len(concerts)} concerts for venue {venue.name}")
+    return concerts
