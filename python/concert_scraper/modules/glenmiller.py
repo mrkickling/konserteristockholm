678a0bb853be36b1efd69e4f289264a5e5357c58
Joakim Loxdal <loxdal@kth.se>
2025-09-22 17:30:19 +0200

    Fix Glen Miller

--- python/concert_scraper/modules/glenmiller.py
+++ python/concert_scraper/modules/glenmiller.py
@@ -2,7 +2,7 @@
 
 from datetime import datetime
 
-from bs4 import BeautifulSoup
+from selenium.webdriver.common.by import By
 
 from ..common import Concert
 from ..logger import get_logger
@@ -16,17 +16,18 @@ def parse_date(date_string):
 def get_concerts(venue, browser):
     logger.info(f"Getting concerts for venue {venue.name}")
     browser.get(venue.url)
-    html = browser.page_source
-
-    soup = BeautifulSoup(html, features="html.parser")
-    cards = soup.find_all(name="div", attrs={'class': 'konsert'})
+    xpath = "/html/body/div/div/div/div/main/div/div/div/div/div/div/div/section/div/div/section/div/div/div/div/div/div/div"
+    concert_elements = browser.find_elements(By.XPATH, xpath)
     concerts = []
-    for card in cards:
-        concert_title = card.find('p', attrs={'class': 'artist'}).getText()
-        concert_date = parse_date(card.find('p', attrs={'class': 'date'}).getText())
+
+    for concert in concert_elements:
+        meta = concert.text.split("\n")
+        artist = "".join(meta[0:-1]) # All before last element is artist
+        date = parse_date(meta[-1]) # Last element is date
         concert_url = venue.url
+
         concerts.append(
-            Concert(concert_title, concert_date, venue.name, concert_url)
+            Concert(artist, date, venue.name, concert_url)
         )
 
     logger.info(f"Found {len(concerts)} concerts for venue {venue.name}")
